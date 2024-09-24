bb6200a281b70069d6d72e0832c3dd511bb5c759
rajapboyevsurojbek <rajapboyevsurojbek@gmail.com>
2024-09-24 23:36:36 +0500

    ,

---
+++ index.js
@@ -0,0 +1,66 @@
+//integer1
+
+let a = +prompt  ("a ni kiriting:");
+
+let metr = parseInt(a / 100);
+
+alert(metr);
+
+//integer2
+
+// let a = +prompt  ("a ni kiriting:");
+
+// let metr = parseInt(a / 1000);
+
+// alert(metr);
+
+//integer3
+
+// let a = +prompt  ("a ni kiriting:");
+
+// let metr = parseInt(a / 1000);
+
+// alert(metr);
+
+//integer4
+
+// let b = +prompt ("b ni kiriting:");
+
+// let c = +prompt ("c ni kiriting:");
+
+// let javob = parseInt(b / c);
+
+// alert(javob);
+
+//integer5
+
+// let b = +prompt("b ni kiriting:");
+
+// let c = +prompt("c ni kiriting:");
+
+// let javob = parseInt(b % c);
+
+// alert(javob);
+
+// begin6
+
+// let a = +prompt("a ni kiruting:");
+
+// let b = +prompt("b ni kiruting:");
+
+// let c = +prompt("c ni kiruting:");
+
+// let p = parseInt(a * b * c);
+
+// let s = 2;
+
+// let javobs = parseInt(2 * a * b + b * c + a * c);
+
+// alert(p);
+
+// alert(javobs);
+
+
+
+
+
